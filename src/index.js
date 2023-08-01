cde1648282be7e491373c0361455c86aac1afc09
Valdes32 <pro100kul@gmail.com>
2023-08-01 14:20:52 +0300

    feat:function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+	let numStr = n.toString();
+  let reversedStr = numStr.split('').reverse().join('');
+  let reversedNumber = parseInt(reversedStr, 10);
+  return reversedNumber;
 }
